cec3f61c3cd10f17071a954116ceb638eedfc867
Nikita-Averchenkov <70913998+Nikita-Averchenkov@users.noreply.github.com>
2021-02-24 22:33:53 +0300

    домашнее задание 3

--- js/main.js
+++ js/main.js
@@ -24,3 +24,52 @@ const getRandomFloatingPoint = function (min, max, decimalDigits) {
   }
 }
 alert(getRandomFloatingPoint(0, 9.9, 3));
+
+
+const types = ['palace', 'flat', 'house', 'bungalow']
+const checks = ['12:00', '13:00', '14:00']
+const titles = ['двухместный номер', 'трёхместный номер', 'номер кинг сайз']
+const rooms = [25, 30, 45]
+const guests = [2, 1]
+
+
+function CreateOffer() {
+  const array = []
+  for (let i = 0; i <= 10; i++) {
+    const offers = {
+      author: {
+        avatar: 'img/avatars/user01.png',
+      },
+      offer: {
+        title: getRandomInteger(titles),
+        address: '100, 200',
+        price: getRandomInteger(10, 100),
+        type: getRandomInteger(types),
+        rooms: getRandomInteger(rooms),
+        guests: getRandomInteger(guests),
+        checkin: getRandomInteger(checks),
+        checkout: getRandomInteger(checks),
+        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+        description: 'small',
+        photos: [
+          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+          'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+      },
+      location: {
+        x: getRandomFloatingPoint(35.65000, 35.70000),
+        y: getRandomFloatingPoint(139.70000, 139.80000),
+      },
+
+    }
+
+    array[i] = offer
+
+
+  }
+
+  return array;
+
+}
+
+const offer = CreateOffer();
