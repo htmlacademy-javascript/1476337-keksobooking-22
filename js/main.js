a263d5d0af23c452d504acfe4619432470e7e50a
Nikita-Averchenkov <70913998+Nikita-Averchenkov@users.noreply.github.com>
2021-03-03 23:02:07 +0300

    добавил функцию возвращающаю массив

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Функция, возвращающая случайное число из переданного диапазона включительно min ... max
 const getRandomInteger = function (min, max) {
   const randomNumber = min + Math.random() * (max - min + 1);
   if (min < 0) {
@@ -11,7 +12,7 @@ const getRandomInteger = function (min, max) {
 
 alert(getRandomInteger(1, 50));
 
-
+// Функция, возвращающая случайное число с плавающей точкой
 const getRandomFloatingPoint = function (min, max, decimalDigits) {
   if (min < 0) {
     return new Error('Диапазон может быть больше или равен нулю');
@@ -31,34 +32,42 @@ const checks = ['12:00', '13:00', '14:00']
 const titles = ['двухместный номер', 'трёхместный номер', 'номер кинг сайз']
 const rooms = [25, 30, 45]
 const guests = [2, 1]
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const description = 'Помещение большое. В нем трое окон и справа от дверей стоят книжные шкафы.';
+const photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-function getRandomElement(items) {
+// Функция, возвращающая случайный элемент массива
+const getRandomElement = (items) => {
 // "|" for a kinda "int div"
   return items[items.length * Math.random() | 0];
 }
 
-const CreateOffer = function () {
+// Функция, возвращающая массив из случайных элементов заданного массива
+const getArrRandomLength = (arr) => {
+  return arr.slice(0, getRandomInteger(0, arr.length - 1));
+};
+
+const createOffer = () => {
   const offers = []
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < 10; i++) {
+    const x = getRandomFloatingPoint(35.65000, 35.70000, 5)
+    const y = getRandomFloatingPoint(139.70000, 139.80000, 5)
     const offer = {
       author: {
-        avatar: 'img/avatars/user0' + i + '.png',
+        avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
       },
       offer: {
-        title: titles[getRandomInteger(0, 2)],
-        address: '100, 200',
+        title: getRandomElement(titles),
+        address: getRandomFloatingPoint(x, y),
         price: getRandomInteger(10, 100),
-        type: types[getRandomElement(3)],
-        rooms: rooms[getRandomElement(25, 30, 45)],
-        guests: guests[getRandomElement(2, 1)],
-        checkin: checks[getRandomElement('12:00', '13:00', '14:00')],
-        checkout: checks[getRandomElement('12:00', '13:00', '14:00')],
-        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-        description: 'small',
-        photos: [
-          'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-          'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-          'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+        type: getRandomElement(types),
+        rooms: getRandomElement(rooms),
+        guests: getRandomElement(guests),
+        checkin: getRandomElement(checks),
+        checkout: getRandomElement(checks),
+        features: getArrRandomLength(features),
+        description: description,
+        photos: getArrRandomLength(photos),
       },
       location: {
         x: getRandomFloatingPoint(35.65000, 35.70000, 5),
@@ -76,5 +85,5 @@ const CreateOffer = function () {
 
 }
 
-const offers = CreateOffer();
+const offers = createOffer();
 console.log(offers);
